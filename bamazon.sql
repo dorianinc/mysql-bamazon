77cfaf8e81d8ae47528f866ceea4fb2d1c662383
Dorian Macias <33275910+dorianinc@users.noreply.github.com>
2018-05-17 18:51:52 -0700

    Update bamazon.sql

--- bamazon.sql
+++ bamazon.sql
@@ -22,14 +22,14 @@ INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES	('Xbox One X 1TB Console', 'Electronics', '484.50', '10'),
         ('PlayStation 4 Pro 1TB', 'Electronics', '379.00', '10'),
         ('Nintendo Switch - Gray', 'Electronics', '299.00', '10'),
-		('Funko POP! Rick and Morty - Tinkles & Ghost in a Jar', 'Toys', '65.00', '10'),
+	('Funko POP! Rick and Morty - Tinkles & Ghost in a Jar', 'Toys', '65.00', '10'),
         ('Funko POP! Rick and Morty - Pickle Rick', 'Toys', '10.99', '10'),
         ('Funko POP! Rick and Morty - Sentient Arm Morty', 'Toys', '10.99', '10'),
         ('Funko POP! Rick and Morty - Scary Terry', 'Toys', '10.99', '10'),
-		('Five Ten Anasazi Moccasym Climbing Shoe', 'Sports & Outdoors', '145.00', '10'),
+	('Five Ten Anasazi Moccasym Climbing Shoe', 'Sports & Outdoors', '145.00', '10'),
         ('Black Diamond Momentum Harness', 'Sports & Outdoors', '45.00', '10'),
         ('Chaandu Blue Tribal Chalk Bag with Belt', 'Sports & Outdoors', '25.99', '10'),
-		('American Journey Dry Dog Food, 4-lb bag', 'Pet Supplies', '49.99', '10'),
+	('American Journey Dry Dog Food, 4-lb bag', 'Pet Supplies', '49.99', '10'),
         ('GREENIES Original Dog Dental Chews Dog Treats, 12 oz', 'Pet Supplies','15.99', '10'),
         ('KONG Wubba Dog Toy LARGE', 'Pet Supplies', '10.99', '10');
 
